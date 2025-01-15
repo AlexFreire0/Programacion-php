dedcb8d68006a17b418653fe260b316453b07980
AlexF <alex.freirebarriga24@campusfp.es>
2025-01-15 13:04:22 +0100

    Pag8

---
+++ Pag8/Ej2.php
@@ -0,0 +1,24 @@
+<?php
+class Tarea {
+    public $nombre;
+    public $descripcion;
+    public $fechaLimite;
+    public $estado;
+    public function marcarComoCompletada() {
+
+    }
+    public function editarDescripcion($nuevaDescripcion) {
+
+    }
+    public function mostrarTarea() {
+        
+    }
+}
+
+
+
+
+
+
+
+?>
--- pruebas/prueba.php
+++ pruebas/prueba.php
@@ -1,20 +1,50 @@
 <?php
-//error_reporting(E_WARNING);
-//error_reporting(0);
-function dividir($a, $b) {
-    if ($b == 0) {
-        throw new Exception("No se puede dividir entre cero.");
+#class coche {
+#    private $sonido;
+#    private $ruedas;
+#
+#    public function __construct() 
+#    {
+#        echo "Mec Mec";
+#        $this->ruedas = 4;
+#    }
+#    public function getRuedas() {
+#        echo "\n". $this->ruedas;
+#    }
+#}
+
+class bicho{
+    private $sonido;
+    private $patas;
+    private $tipobicho;
+
+    public function __construct($sonido_ext, $patas_ext, $tipobicho_ext){
+        $this->sonido = $sonido_ext;
+        $this->patas = $patas_ext;
+        $this->tipobicho= $tipobicho_ext;
+        
     }
-    return $a / $b;
+    public function verdetalles()
+    {
+        echo "sonido: ". $this->sonido. "\npatas: ". $this->patas. "\nTipo bicho: ". $this->tipobicho;
+    }
 }
+class perro extends bicho {
 
-try {
-    echo dividir(10, 0);
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
+    private $frase;
+
+    public function __construct($sonido_ext,$patas_ext, $tipobicho_ext, $frase_ext)
+    {
+        $this->frase = $frase_ext; 
+    }
+    public function verdetalles()
+    {
+        parent::verdetalles();
+        echo $this->frase;
+    }
 }
+$miperro = new bicho("guau", 4, "Perro");
+$miperro-> verdetalles();
 
-
-
-
+$paco = new perro;
 ?>
